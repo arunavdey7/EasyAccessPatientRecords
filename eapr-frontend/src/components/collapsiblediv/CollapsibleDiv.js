ba17afadaefee017cae50c9d8814c19b2247d0e2
Arunav Dey <arunav.dey.7@gmail.com>
2022-01-12 09:48:09 +0530

    Design login page

--- eapr-frontend/src/components/collapsiblediv/CollapsibleDiv.js
+++ eapr-frontend/src/components/collapsiblediv/CollapsibleDiv.js
@@ -1,21 +1,10 @@
-import React,{useState} from 'react' 
-import './styles.css'
+import React,{useState} from 'react'
+
+
 
 const CollapsibleDiv = () => {
-    const [styling, setStyling] = useState({display:"none"})
-    const handleExpansion = () => {
-        if(styling.display==="none")
-            setStyling({display:"flex"})
-        else
-            setStyling({display:"none"})
-    }
     return(
-        <div className='collapsible_div'>
-            <div onClick={handleExpansion} className='visible_part'></div>
-            <div className='hidden_part' style={styling}>
-                
-            </div>
-        </div>
+        <div></div>
     )
 }
 export default CollapsibleDiv;
