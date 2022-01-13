8070cefc2aaca1876203bef980f75813f8941069
Arunav Dey <arunav.dey.7@gmail.com>
2022-01-13 23:22:29 +0530

    WIP: Creation of intermediatary pages

--- eapr-frontend/src/pages/registeration/Registration.js
+++ eapr-frontend/src/pages/registeration/Registration.js
@@ -9,16 +9,21 @@ const Registration = () => {
 
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const [specialization, setSpecialization] = useState("");
     const [role, setRole] = useState("No role selected")
+    
 
     const handleRole = (event) => {
         var selected = event.target.id
         if(selected === 'role_doctor')
+        {
             setRole('Doctor')
+        }
         else if(selected === 'role_patient')
+        {
             setRole('Patient')
-        else
-            setRole('Admin')
+        }
+            
     }
     console.log(role)
     const handleSignup = () =>
@@ -37,13 +42,20 @@ const Registration = () => {
                     <div className="role_container">
                         <div onClick={handleRole} id='role_doctor' className="role_card"></div>
                         <div onClick={handleRole} id='role_patient' className="role_card"></div>
-                        <div onClick={handleRole} id='role_admin' className="role_card"></div>
                     </div>
                    <div className="inner-container-y">
                        
                         <div className="login_form_container">
                             <div className="selected_role">
                                 <p>Selected Role: </p><h2>{role}</h2>
+                            </div>
+                            {role === 'Doctor' ?
+                            <>
+                            <div className='credential_labels'>
+                                <label for='email'>Name</label>
+                            </div>  
+                            <div>
+                                <input className='input_fields' onChange={e => setEmail(e.target.value)} type='email' name="email"/>
                             </div>
                             <div className='credential_labels'>
                                 <label for='email'>Email</label>
@@ -57,6 +69,54 @@ const Registration = () => {
                             <div>
                                 <input className='input_fields' onChange={e => setPassword(e.target.value)} type='password' name='password'/>
                             </div>
+                            <div className='credential_labels'>
+                                <label for='specialization'>Specialization</label>
+                            </div>  
+                            <div>
+                                <input className='input_fields' onChange={e => setSpecialization(e.target.value)} type='text' name='specialization'/>
+                            </div>
+                            </>
+                            : 
+                            <>
+                            <div className='credential_labels'>
+                                <label for='email'>Name</label>
+                            </div>  
+                            <div>
+                                <input className='input_fields' onChange={e => setEmail(e.target.value)} type='email' name="email"/>
+                            </div>
+                            <div className='credential_labels'>
+                                <label for='email'>Age</label>
+                            </div>  
+                            <div>
+                                <input type='number' className='input_fields' onChange={e => setEmail(e.target.value)} type='email' name="email"/>
+                            </div>
+                            <div className='credential_labels'>
+                                <label for='email'>Address</label>
+                            </div>  
+                            <div>
+                                <textarea type='a' className='input_fields' onChange={e => setEmail(e.target.value)}  name="address"/>
+                            </div>
+                            
+                            <div className='credential_labels'>
+                                <label for='email'>Email</label>
+                            </div>  
+                            <div>
+                                <input className='input_fields' onChange={e => setEmail(e.target.value)} type='email' name="email"/>
+                            </div>
+                            <div className='credential_labels'>
+                                <label for='password'>Password</label>
+                            </div>
+                            <div>
+                                <input className='input_fields' onChange={e => setPassword(e.target.value)} type='password' name='password'/>
+                            </div>
+                            <div className='credential_labels'>
+                                <label for='specialization'>Specialization</label>
+                            </div>  
+                            <div>
+                                <input className='input_fields' onChange={e => setSpecialization(e.target.value)} type='text' name='specialization'/>
+                            </div>
+                            </>
+                            }           
                             <div className="login_btn_container">
                                 <div>
                                     <button className="login_btn">Register
