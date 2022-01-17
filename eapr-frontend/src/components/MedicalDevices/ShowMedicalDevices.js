6f85064282be8519d431acda6d89eff196540d68
Varun Kumar <dhakavarun12@gmail.com>
2022-01-18 00:02:36 +0530

    frontend Show Forms

--- eapr-frontend/src/components/MedicalDevices/ShowMedicalDevices.js
+++ eapr-frontend/src/components/MedicalDevices/ShowMedicalDevices.js
@@ -27,38 +27,38 @@ const ShowMedicalDevices = () => {
             <h1>Device use statement</h1>
             <h2>Data</h2>
             <h2>Device Details</h2>
-            <label>Device name</label>
+            <label>Device name : <span className='values'>{data["device_name"]}</span></label>
             <br/>
-            <label>Body site</label>
+            <label>Body site : <span className='values'>{data["body_site"]}</span></label>
             <br/>
             <h2>Medical Device</h2>
-            <label>Device name</label>
+            <label>Device name : <span className='values'>{data["device_name"]}</span></label>
             <br/>
-            <label>Type</label>
+            <label>Type : <span className='values'>{data["type"]}</span></label>
             <br/>
-            <label>Description</label>
+            <label>Description : <span className='values'>{data["description"]}</span></label>
             <br/>
-            <label>Unique Device Identifier (UDI)</label>
+            <label>Unique Device Identifier (UDI) : <span className='values'>{data["UDI"]}</span></label>
             <br/>
-            <label>Manufacturer</label>
+            <label>Manufacturer : <span className='values'>{data["manufacturer"]}</span></label>
             <br/>
-            <label>Date of manufacture</label>
+            <label>Date of manufacture : <span className='values'>{data["date_of_manufacture"]}</span></label>
             <br/>
-            <label>Serial number</label>
+            <label>Serial number : <span className='values'>{data["serial_number"]}</span></label>
             <br/>
-            <label>Catalogue number</label>
+            <label>Catalogue number : <span className='values'>{data["catalogue_number"]}</span></label>
             <br/>
-            <label>Model number</label>
+            <label>Model number : <span className='values'>{data["model_number"]}</span></label>
             <br/>
-            <label>Batch/Lot number</label>
+            <label>Batch/Lot number : <span className='values'>{data["batch_number"]}</span></label>
             <br/>
-            <label>Software version</label>
+            <label>Software version : <span className='values'>{data["software_version"]}</span></label>
             <br/>
-            <label>Date of expiry</label>
+            <label>Date of expiry : <span className='values'>{data["date_of_expiry"]}</span></label>
             <br/>
-            <label>Other identifier</label>
+            <label>Other identifier : <span className='values'>{data["other_identifier"]}</span></label>
             <br/>
-            <label>Comment</label>
+            <label>Comment : <span className='values'>{data["comment"]}</span></label>
             <br/>
         </div>
         </>
