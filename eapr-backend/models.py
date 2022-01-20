4250772a7f5a5c42acdd953318de016600b294f7
Ajay K Gupta <ajay.k.gupta@innovaccer.com>
2022-01-20 14:17:50 +0530

    added migrations

--- eapr-backend/models.py
+++ eapr-backend/models.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost/sample6"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:pass@localhost/sample6"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 #1. Model for patient Details
 class Patient_details(db.Model):
